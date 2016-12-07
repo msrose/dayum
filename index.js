a9378ee19b1dee0c6dd18c768bcdf85f9435dc64
Michael Rose <michael_rose@outlook.com>
2016-12-06 21:12:10 -0800

    Add dayumify functionality

--- index.js
+++ index.js
@@ -1,14 +1,44 @@
-const dayum = (count = 1) => {
-  let d = 'da';
+'use strict';
+
+let dayum = (count = 1) => {
+  let tsd = 'da';
   while(count-- > 1) {
-    d += 'a';
+    tsd += 'a';
   }
-  d += 'yum';
-  return  d;
+  tsd += 'yum';
+  return tsd;
 };
 
 dayum.random = (max = 100) => {
   return dayum(Math.floor(Math.random() * max) + 1);
 };
+
+const dayumRegex = /^d(a+)yum$/;
+
+dayum.dayumify = (object, count = 0) => {
+  if(count === 0 && Proxy) {
+    return new Proxy(object, {
+      get: (obj, prop, value) => {
+        const result = dayumRegex.exec(String(prop))
+        if(result) {
+          const [, count] = result;
+          return dayum.random.bind(count);
+        }
+        return obj[prop];
+      }
+    });
+  } else {
+    if(count === 0) {
+      count = 50;
+    }
+    for(let i = 1; i <= count; i++) {
+      const tsd = dayum(i);
+      object[tsd] = dayum.random.bind(null, i);
+    }
+    return object;
+  }
+};
+
+dayum = dayum.dayumify(dayum);
 
 module.exports = dayum;
