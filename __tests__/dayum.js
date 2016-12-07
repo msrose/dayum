30fbd0df4573c3f2dd97241c0ea9dcf3c3fd7b84
Michael Rose <michael_rose@outlook.com>
2016-12-06 21:12:10 -0800

    Add dayumify functionality

--- __tests__/dayum.js
+++ __tests__/dayum.js
@@ -8,10 +8,37 @@ describe('dayum', () => {
     expect(dayum(-1)).toBe('dayum');
   });
 
-  it('generates a random dayum', () => {
-    expect(dayum.random(1)).toBe('dayum');
-    expect(dayum.random()).toMatch(/^da+yum$/);
-    expect(dayum.random(30)).toMatch(/^da+yum$/);
-    expect(dayum.random(-30)).toMatch(/^da+yum$/);
+  it('has all the dayum methods you could want', () => {
+    expect(dayum.dayum).toBeInstanceOf(Function);
+    expect(dayum.daayum).toBeInstanceOf(Function);
+    expect(dayum.daaaaaaaaaayum).toBeInstanceOf(Function);
+    expect(dayum.daaaaaaaaaaaaaaaaaaaaayum).toBeInstanceOf(Function);
+    expect(dayum.daaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaayum).toBeInstanceOf(Function);
+    expect(dayum.daaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaayum).toBeInstanceOf(Function);
+  });
+
+  describe('random', () => {
+    it('generates a random dayum', () => {
+      expect(dayum.random(1)).toBe('dayum');
+      expect(dayum.random()).toMatch(/^da+yum$/);
+      expect(dayum.random(30)).toMatch(/^da+yum$/);
+      expect(dayum.random(-30)).toMatch(/^da+yum$/);
+    });
+  });
+
+  describe('dayumify', () => {
+    it('adds all the dayum methods to a given object', () => {
+      const obj = dayum.dayumify({}, 10);
+      expect(obj.dayum()).toBe('dayum');
+      expect(obj.daaayum).toBeInstanceOf(Function);
+      expect(obj.daaaaaaaaaayum).toBeInstanceOf(Function);
+      expect(obj.daaaaaaaaaaayum).toBeUndefined();
+    });
+
+    it('returns a dayum Proxy if no count is given', () => {
+      const obj = dayum.dayumify({ x: 23 });
+      expect(obj.x).toBe(23);
+      expect(obj.daaaaaaaaaaaaaaaaaaaaaayum).toBeInstanceOf(Function);
+    });
   });
 });
